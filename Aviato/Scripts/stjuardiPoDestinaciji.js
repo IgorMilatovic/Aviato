85f6580935f3508ca0a37e92a7830d76ee243dd6
IgorMilatovic <igby.veko@gmail.com>
2019-07-04 14:58:23 +0200

    added project

---
+++ Aviato/Scripts/stjuardiPoDestinaciji.js
@@ -0,0 +1,11 @@
+﻿
+
+//window.onload = () => {
+
+    var destinacija = document.querySelector("#Destinacija");
+    
+    //$(".stjuardi").load(`/Let/StjuardiPoDestinaciji?id=${destinacija.value}`);
+    destinacija.addEventListener("change", function () {
+        $(".stjuardi").load(`/Let/StjuardiPoDestinaciji?id=${destinacija.value}`);
+    })
+//}
---
+++ Aviato/Scripts/stjuardiPoDestinacijiEdit.js
@@ -0,0 +1,10 @@
+﻿
+//window.onload = () => {
+
+    var destinacija = document.querySelector("#Destinacija");
+
+//    $(".stjuardi").load(`/Let/StjuardiPoDestinaciji?id=${destinacija.value}`);
+    destinacija.addEventListener("change", function () {
+        $(".stjuardi").load(`/Let/StjuardiPoDestinaciji?id=${destinacija.value}`);
+    })
+//}
